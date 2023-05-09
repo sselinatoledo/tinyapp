8083e3bd9053c65e61a726b74b0ebfb968d785b5
sselinatoledo <sselinatoledo@gmail.com>
2023-05-09 09:05:03 +0000

    working on submitting the new URL from editing

--- express_server.js
+++ express_server.js
@@ -1,64 +1,84 @@
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
-const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-};
-
+// 4. Adding the middleware to parse the request body and make it readble.
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
-
-app.get("/urls/:id", (req, res) => {
-    const shortURL = req.params.id;
-    const longURL = urlDatabase[shortURL];
-    const templateVars = { shortURL, longURL }
-    res.render("urls_show", templateVars);
-  });
-
-app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get("/", (req, res) => {
-    res.send("Hello!");
+  res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-const generateRandomString = () => {
-  return Math.random().toString(36).substring(2,8)
-}
+// 1. SENDING DATA TO urls_index.ejs.
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
+// 3. CREATING NEW URL.
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// 2. ADDING A SECOND ROUTE AND TEMPLATE.
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+// 5. RESPOND TO THE FORM SUBMISSION WITH A CONFIRMATION MESSAGE. ADD NEW KEY-VALUE PAIR WHERE shortURL is key and longURL is value.
 app.post("/urls", (req, res) => {
-    const longURL = req.body.longURL;
-    const shortURL = generateRandomString();
-    urlDatabase[shortURL] = longURL;
-    res.redirect(`/urls/${shortURL}`)
-  });
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
-  app.get("/u/:id", (req, res) => {
-    const longURL = urlDatabase[req.params.id];
-    if (longURL) {
-      res.redirect(longURL);
-    } else {
-      res.status(404).send("Short URL not found");
-    }
-  });
+// 6. REDIRECT TO ITS longURL.
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+// 7. REMOVES A URL resource.
+app.post('/urls/:id/delete', (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect('/urls');
+});
+
+// 8. when you edit it and submit the new url 
+app.post('/urls/:id', (req, res) => {
+  const { id } = req.params;
+  const newLongURL = req.body.longURL;
+  urlDatabase[id] = newLongURL;
+  res.redirect('/urls');
+});
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
