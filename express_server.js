0161b83220a9f31e2db262387dfd42552129f79e
sselinatoledo <sselinatoledo@gmail.com>
2023-07-29 16:46:57 +0000

    new log in page

--- express_server.js
+++ express_server.js
@@ -90,7 +90,7 @@ app.get('/register', (req, res) => {
 
 app.post('/logout', (req, res) => {
     res.clearCookie('user_id');
-    res.redirect('/urls');
+    res.redirect('/login');
   });
 
 app.post('/urls/:id', (req, res) => {
